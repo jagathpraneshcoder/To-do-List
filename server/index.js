f575d8b49864cf0f51db82b6b5a28c9d3dcc1424
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-03-23 13:32:54 +0530

    todo_getting the list

--- server/index.js
+++ server/index.js
@@ -14,16 +14,47 @@ app.post("/todos",async(req,res)=>{
   try{
     const {description} = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1)",
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
 
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
     res.sendStatus(200);
   }catch(err){
     console.error(err.message);
   }
   
+})
+
+//to get all todo
+app.get("/todos",async (req,res)=>{
+  try {
+    const allTodo = await pool.query("SELECT * FROM todo");
+    console.log(allTodo);
+    res.json(allTodo.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+})
+
+//to get a todo
+app.get("/todos/:id",async (req,res)=>{
+  try {
+    const {id} = req.params;
+    const todo = await pool.query(
+        "SELECT description FROM todo WHERE todo_id = $1",
+        [id]
+      );
+    if(todo.rows.length > 0){
+      res.json(todo.rows);
+    }
+    else{
+      res.json("description unavailable");
+    }
+  } catch (error) {
+      console.log(error.message);
+      res.json(error);
+  }
 })
 
 app.listen(5000, () => {
